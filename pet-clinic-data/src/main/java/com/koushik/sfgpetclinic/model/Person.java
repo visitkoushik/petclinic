b61334268b6daa239f45e865fe2550cd30bf561f
visitkoushik <visit.koushik@gmail.com>
2022-10-27 21:12:41 +0530

    Add Id to model object. Close #10

--- pet-clinic-data/src/main/java/com/koushik/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/koushik/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.koushik.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/koushik/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/koushik/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.koushik.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
