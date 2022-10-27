ba87fbbcafe63272793c7a2e27896d3023315a69
visitkoushik <visit.koushik@gmail.com>
2022-10-27 21:14:15 +0530

    Add Id to model object. Close #10

--- pet-clinic-data/src/main/java/com/koushik/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/koushik/sfgpetclinic/model/Pet.java
@@ -2,7 +2,8 @@ package com.koushik.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity{
+public class Pet extends BaseEntity {
+
     private PetType petType;
     private Owner owner;
     private LocalDate birthDay;
