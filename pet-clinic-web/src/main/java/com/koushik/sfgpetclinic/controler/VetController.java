d84925d777960f8e34a2b948103f6dccb60081d7
visitkoushik <visit.koushik@gmail.com>
2022-10-29 21:02:37 +0530

    List All vets. close #16

--- pet-clinic-web/src/main/java/com/koushik/sfgpetclinic/controler/VetController.java
+++ pet-clinic-web/src/main/java/com/koushik/sfgpetclinic/controler/VetController.java
@@ -1,16 +1,28 @@
 package com.koushik.sfgpetclinic.controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.koushik.sfgpetclinic.services.VetService;
 
 @RequestMapping("vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
 
     @RequestMapping({"","/","/index","/index.html"})
-    public String inddex(){
-
+    public String listVet(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
