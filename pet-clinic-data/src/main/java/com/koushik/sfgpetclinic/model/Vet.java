43ad0a5e8266d97b8df7546f5daf0fdacc54d89e
visitkoushik <visit.koushik@gmail.com>
2022-11-10 12:40:25 +0530

    Update Vet to entity. close #35

--- pet-clinic-data/src/main/java/com/koushik/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/koushik/sfgpetclinic/model/Vet.java
@@ -5,8 +5,23 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @Column(name = "specialities")
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name="vet_speciality",
+    joinColumns = @JoinColumn(name="vet_id"),
+    inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
